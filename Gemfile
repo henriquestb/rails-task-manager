92cefef7fe3850c2f82fc1ee49d2577d8ab4aceb
Henrique Starling <henrique_st@yahoo.com.br>
2020-04-18 14:56:45 -0300

    lets go

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby '2.6.5'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
+gem 'bootstrap-forms'
+gem 'font-awesome-sass'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get '/list', to: 'Tasks#list'
+  # get '/tasks', to: 'tasks#list'
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: 'tasks#show', as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :update_task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
